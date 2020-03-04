4885c3498a276c643b1c67eebd05fb8c724e8dc3
Jonathan Mucha <jonmucha@gmail.com>
2020-03-03 19:24:25 -0500

    added firebase

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-# import requests
-
+import firebase_admin
+from firebase_admin import credentials
+from google.cloud import firestore
+from google.cloud.firestore_v1.field_path import FieldPath
+cred = credentials.Certificate(
+    "./firebase-admin.json")
+firebase_admin.initialize_app(cred)
 app = FastAPI()
 
 
-class Address(BaseModel):
+class TrafficAddress(BaseModel):
     """
         Desccribes an address to drive robotic traffic to \n
         client_id is an integer \n
@@ -14,36 +19,84 @@ class Address(BaseModel):
     """
     client_id: int
     client: str
-    store_address: str
+    address: str
 
 
-@app.get("/addresses/{client_id}")
+class OrderAddress(BaseModel):
+    """
+        Desccribes an address to drive robotic traffic to \n
+        client_id is an integer \n
+        client: string \n
+        store_address: string
+    """
+    client_id: int
+    client: str
+    address: str
+
+
+class Client(BaseModel):
+    """
+        the client object used in database
+    """
+    client_name: str
+
+
+@app.get("/clients/{client_id}")
 def update_item(client_id: int):
     """
-        this is a cool docstring
+        docstring for get(client_id)
     """
-    obj = {'client_id': address.client_id, "client": address.client,
-           "store_address": address.store_address}
-    print("This object will be created: ", obj)
-    print("now making request to the associated address")
-    print(requests.get(obj["store_address"]).status_code)
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--window-size=1024,768')
-    chrome_options.add_argument('--no-sandbox')
-
-    browser = webdriver.Chrome(chrome_options=chrome_options)
-    browser.get(obj['store_address'])
-    browser.quit()
+    # TODO: retreive a client from database and return as json object
 
     return {"hello": "World"}
 
 
-@app.put("/addresses/{client_id}")
-def update_item(client_id: int, address: Address):
+@app.post("/clients/")
+def update_item(client: Client):
+    """
+        docstring for post() new client
+    """
+    # TODO: create a client and add to database, return as sucess json object
+    # add a new user document????
+    db = firestore.Client()
+    doc_ref = db.collection(u'clients').document(client.client_name)
+    doc_ref.set({
+        u'client_name': client.client_name
+    })
+    # print(db.collection(u'clients').document(
+    #     client.client_name).get('client_name'))
+    fp = FieldPath()
+    print(client.client_name)
+    # path = fp.from_string(
+    #     'clients.' + str(client.client_name) + '.client_name')
+    # print(path)
+    # return {"Client": db.collection(u'clients').document(client.client_name).get()}
+    # return {"Something": "MIght have HAapenED"}
+
+
+@app.put("/clients/{client_id}")
+def update_item(client_id: int, client: Client):
+    """
+        docstring for put(client_id)
+    """
+    # TODO: retreive a client from database and update it
+
+    return {"Update Status": "Complete"}
+
+
+@app.delete("/clients/{client_id}")
+def update_item(client_id: int, client: Client):
+    """
+        docstring for delete(client_id)
+    """
+    # TODO: retreive a client from database and delete it
+    return {"Delete Status": "Success"}
+
+
+@app.get("/traffic/{client_id}")
+def update_item(client_id: int, address: TrafficAddress):
     obj = {'client_id': address.client_id, "client": address.client,
-           "store_address": address.store_address}
+           "store_address": address.address}
     print("This object will be created: ", obj)
     print("now making request to the associated address")
 
