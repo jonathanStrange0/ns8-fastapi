d94d5556d6ca88a3e4d1a61aed534151b87e9e77
Jonathan Mucha <jonmucha@gmail.com>
2020-05-19 14:38:07 -0400

    fixed error in push dke script

--- push_update_to_gke.sh
+++ push_update_to_gke.sh
@@ -3,10 +3,10 @@
 # run this script after pulling most recent version of repo
 # Only run this script on gcloud console
 
-
+gcloud config set project ns8-traffic-api &&
 
 gcloud config set compute/zone us-central1-c &&
 gcloud container clusters get-credentials api-cluster-2 &&
 docker build -t gcr.io/ns8-traffic-api/ns8-traffic-api:latest . &&
-docker push gcr.io/ns8-traffic-api/traffic-api:latest &&
-kubectl set image deployment/api-app traffic-api=gcr.io/ns8-traffic-api/traffic-api:latest
+docker push gcr.io/ns8-traffic-api/ns8-traffic-api:latest &&
+kubectl set image deployment/api-app traffic-api=gcr.io/ns8-traffic-api/ns8-traffic-api:latest
